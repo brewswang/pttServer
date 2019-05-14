45f1e30e2ecd2ce29226cbde218f7c18d130c210
Yabets <yabetsbelay@gmail.com>
2019-05-14 14:39:29 +0300

    change HttpServlet to prevent build failure from common

--- src/main/java/com/dventus/ptt/servlet/PttServer.java
+++ src/main/java/com/dventus/ptt/servlet/PttServer.java
@@ -3,24 +3,44 @@ package com.dventus.ptt.servlet;
 import com.dventus.ptt.api.PTTRequest;
 import com.dventus.ptt.api.PTTResponse;
 import com.dventus.ptt.api.Processor;
-import com.dventus.servlet.DventusServlet;
 import com.google.gson.Gson;
 import org.apache.http.entity.ContentType;
 import org.apache.log4j.Logger;
+import org.eclipse.jdt.annotation.Nullable;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.annotation.CheckForNull;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
 
-public class PttServer extends DventusServlet {
+public class PttServer  extends HttpServlet {
 
     transient static private final Logger logger = Logger.getLogger(PttServer.class);
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2134720152156409150L;
+
+    transient @CheckForNull @Nullable protected ApplicationContext webApplicationContext;
+
+    @CheckForNull @Nullable protected ApplicationContext getWebApplicationContext() {
+        return this.webApplicationContext;
+    }
+
+    protected void setWebApplicationContext( ApplicationContext webApplicationContext1) {
+        this.webApplicationContext = (WebApplicationContext) webApplicationContext1;
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.setWebApplicationContext(WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext()));
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
